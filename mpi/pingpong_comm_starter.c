3971e5a132e2830c11e02b228c639683b04d008a
Madhav Poudel <king.ov.universe@gmail.com>
2020-02-10 17:18:06 -0700

    Ping pong implemented

--- mpi/pingpong_comm_starter.c
+++ mpi/pingpong_comm_starter.c
@@ -30,11 +30,50 @@ int main(int argc, char **argv) {
     return 0;
   }
 
-  //Write code here
-  printf("Hello from %d \n", my_rank);
+  // Write code here
 
+  int ping_pong_limit = 5;
 
+  // Counter of a rank
+  int ping_pong_count = 0;
+
+  // 5 times back and forth process
+  for(int i = 0; i < ping_pong_limit; i++) {
+    
+    if(my_rank % 2 == 0) {
+
+      int to_increment;
+      MPI_Status status;
+
+      MPI_Send(&my_rank, 1, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD);
+
+      MPI_Recv(&to_increment, 1, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD, &status);
+      
+      // Update the counter
+      ping_pong_count += to_increment;
+
+    } else {
+
+      int to_increment;
+      MPI_Status status;
+
+      MPI_Recv(&to_increment, 1, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD, &status);
+      
+      // Update the counter
+      ping_pong_count += to_increment;
+      
+      MPI_Send(&my_rank, 1, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD);
+
+    }
+  }
+  
+  printf("\n My Rank is %d and ping pong count is %d \n", my_rank, ping_pong_count);
 
   MPI_Finalize();
   return 0;
 }
+
+/* *
+ * Q1: What does process rank 5’s counter store at the end of the computation?
+ * Ans: 20
+ */
