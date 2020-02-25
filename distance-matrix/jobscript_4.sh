9176bc815e4a3df72b0cc4df039dccada6d0c1ce
Madhav Poudel <king.ov.universe@gmail.com>
2020-02-25 15:04:54 -0700

    Added more jobscripts

---
+++ distance-matrix/jobscript_4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#BATCH --job-name=distance_matrix_metrics        
+#SBATCH --output=/scratch/mp2525/distance_matrix_metrics.txt	
+#SBATCH --error=/scratch/mp2525/distance_matrix_metrics.err
+#SBATCH --time=20:00
+#SBATCH --mem=2000 
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=1
+
+module load openmpi
+
+mpicc -O3 distance_matrix_metrics.c -lm -o distance_matrix_metrics.out -std=c99
+
+srun distance_matrix_metrics.out 10000 90 MSD_year_prediction_normalize_0_1_100k.txt
