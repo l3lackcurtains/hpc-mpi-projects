f34e91e8b51af663fc4566cc1101c7f2020307e2
Madhav Poudel <king.ov.universe@gmail.com>
2020-02-25 19:25:10 -0700

    adjust renaming of metrics

--- distance-matrix/activity2/jobscript_cache_3000.sh
+++ distance-matrix/activity2/jobscript_cache_3000.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#BATCH --job-name=distance_matrix_metrices_cache.c       
-#SBATCH --output=/scratch/mp2525/distance_matrix_metrices_cache.c.txt	
-#SBATCH --error=/scratch/mp2525/distance_matrix_metrices_cache.c.err
+#BATCH --job-name=distance_matrix_metrics_cache       
+#SBATCH --output=/scratch/mp2525/distance_matrix_metrics_cache.txt	
+#SBATCH --error=/scratch/mp2525/distance_matrix_metrics_cache.err
 #SBATCH --time=02:00
 #SBATCH --mem=2000 
 #SBATCH --nodes=1
@@ -11,6 +11,6 @@
 
 module load openmpi
 
-mpicc -O3 distance_matrix_metrices_cache.c.c -lm -o distance_matrix_metrices_cache.c.out -std=c99
+mpicc -O3 distance_matrix_metrics_cache.c.c -lm -o distance_matrix_metrics_cache.c.out -std=c99
 
-srun distance_matrix_metrices_cache.c.out 10000 90 3000 MSD_year_prediction_normalize_0_1_100k.txt
+srun distance_matrix_metrics_cache.c.out 10000 90 3000 MSD_year_prediction_normalize_0_1_100k.txt
