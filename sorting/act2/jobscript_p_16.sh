c5092a2b66e531bf3dc586dbef3dda87317f3eb2
Madhav Poudel <king.ov.universe@gmail.com>
2020-03-05 11:14:24 -0700

    Updated bash files

---
+++ sorting/act2/jobscript_p_16.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#BATCH --job-name=sort_act2_mp2525
+#SBATCH --output=/scratch/mp2525/sort_act2_mp2525.txt	
+#SBATCH --error=/scratch/mp2525/sort_act2_mp2525.err
+#SBATCH --time=05:00
+#SBATCH --mem=4000
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --cpus-per-task=1
+#SBATCH --exclusive
+
+module load openmpi
+
+mpicc -O3 sort_act2_mp2525.c -lm -o sort_act2_mp2525.out -std=c99
+
+srun ./sort_act2_mp2525.out
