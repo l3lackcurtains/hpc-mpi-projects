06088e46fd356cce7cf61c02806b8cb4ecff3689
Madhav Poudel <king.ov.universe@gmail.com>
2020-04-09 21:41:41 -0700

    Added jobscript for two nodes

---
+++ k-means/act1-2nodes/jobscript_p_16_k_2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -C sl
+#BATCH --job-name=kmeans_act1_mp2525_p16_k2
+#SBATCH --output=/scratch/mp2525/kmeans_act1_mp2525_p16_k2.txt
+#SBATCH --error=/scratch/mp2525/kmeans_act1_mp2525_p16_k2.err
+#SBATCH --time=30:00
+#SBATCH --mem=0
+#SBATCH --nodes=2
+#SBATCH --ntasks=16
+#SBATCH --ntasks-per-node=8
+#SBATCH --exclusive
+
+module load openmpi
+
+mpicc -O3 kmeans_act1_mp2525.c -lm -o kmeans_act1_mp2525.out -std=c99
+
+srun ./kmeans_act1_mp2525.out 5159737 2 2 ../iono_57min_5.16Mpts_2D.txt
---
+++ k-means/act1-2nodes/jobscript_p_4_k_50.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -C sl
+#BATCH --job-name=kmeans_act1_mp2525_p4_k50
+#SBATCH --output=/scratch/mp2525/kmeans_act1_mp2525_p4_k50.txt	
+#SBATCH --error=/scratch/mp2525/kmeans_act1_mp2525_p4_k50.err
+#SBATCH --time=30:00
+#SBATCH --mem=0
+#SBATCH --nodes=2
+#SBATCH --ntasks=4
+#SBATCH --ntasks-per-node=2
+#SBATCH --exclusive
+
+module load openmpi
+
+mpicc -O3 kmeans_act1_mp2525.c -lm -o kmeans_act1_mp2525.out -std=c99
+
+srun ./kmeans_act1_mp2525.out 5159737 2 50 ../iono_57min_5.16Mpts_2D.txt
